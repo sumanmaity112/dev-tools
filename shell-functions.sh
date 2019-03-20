5d7110ec16cb10f16729991960c3e5d76796aba9
sumanmaity112 <suman.maity112@gmail.com>
2019-03-20 23:09:37 +0530

    Add pull-all command to take latest code from all the git repositories present in current directory

---
+++ shell-functions.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+pull-all(){
+    for subDir in `ls -d */`; do
+        if [[ -d ${subDir}/.git ]]; then
+            tput bold; tput setaf 2; echo "\033[4m$subDir\033[0m"
+            (cd ${subDir} && git pull --rebase)
+        fi
+    done
+}
