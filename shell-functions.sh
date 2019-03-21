2e3eb38a7a4a2115e4a732d65ff0f1d630b37e73
sumanmaity112 <suman.maity112@gmail.com>
2019-03-21 10:56:59 +0530

    Add lc-clone command

--- shell-functions.sh
+++ shell-functions.sh
@@ -8,3 +8,40 @@ pull-all(){
         fi
     done
 }
+
+lc-clone(){
+    CONFIG_FILE_NAME=./.config.json
+    if [[ ! -f ${CONFIG_FILE_NAME} ]]; then
+        echo "Config file is not found!. Please create it. For more info check https://github.com/sumanmaity112/dev-tools#dev-tools"
+        return 1;
+    fi
+
+    GIT_USERNAME=$(jq -r .git_username ${CONFIG_FILE_NAME})
+    COMMITTER_NAME=$(jq -r .committer_name ${CONFIG_FILE_NAME})
+    EMAIL=$(jq -r .email ${CONFIG_FILE_NAME})
+    SIGNKEY=$(jq -r .signkey ${CONFIG_FILE_NAME})
+
+    URL=$1
+    REPO_NAME=$(basename "$URL" ".${URL##*.}")
+
+    TARGET_DIR=${2:-${REPO_NAME}}
+
+    NEW_URL="git@github.com-$GIT_USERNAME:$GIT_USERNAME/$REPO_NAME.git"
+
+    git clone ${NEW_URL} ${TARGET_DIR}
+
+    if [[ $? = 0 ]]
+    then
+        # set up git user details and sign key
+
+        pushd ${TARGET_DIR} > /dev/null
+            echo $(pwd)
+            git config user.name ${COMMITTER_NAME}
+            git config user.email ${EMAIL}
+            git config user.signingkey ${SIGNKEY}
+
+        popd > /dev/null
+    else
+        return $?
+    fi
+}
